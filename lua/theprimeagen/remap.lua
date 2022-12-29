620a7802a09756c5735baab138568311c1805c4f
Korimis <quentinradlo.pro@gmail.com>
2022-12-29 14:17:41 +0100

    Few changes for better console experience while in neovim

--- lua/theprimeagen/remap.lua
+++ lua/theprimeagen/remap.lua
@@ -10,3 +10,16 @@ vim.keymap.set("n", "<C-s>", vim.cmd.w)
 vim.keymap.set("n", "<leader>y", "\"+y")
 vim.keymap.set("v", "<leader>y", "\"+y")
 vim.keymap.set("n", "<leader>Y", "\"+Y")
+
+-- Opens a terminal
+vim.api.nvim_set_keymap("n", "<S-t>", ":terminal<CR>", {noremap = true})
+
+-- Enter Insert mode directly when opening a terminal
+vim.api.nvim_command("augroup terminal")
+vim.api.nvim_command("  au TermOpen * startinsert")
+vim.api.nvim_command("augroup END")
+
+-- map jk to escape terminal
+vim.api.nvim_set_keymap("i", "jk", "<Esc>", {noremap = true})
+vim.api.nvim_set_keymap("t", "jk", "<C-\\><C-n>", {noremap = true})
+
